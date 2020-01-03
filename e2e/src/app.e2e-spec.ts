d5710b9f1d7e5515d85e40b85d1c8fbd0f53d8b2
whs <hswongac@gmail.com>
2020-01-03 13:24:16 +0800

    Fix tests

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('seekers-greeting app is running!');
+    expect(page.getGreetingText()).toContain('Greetings Seekers Capital!');
   });
 
   afterEach(async () => {
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,7 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getGreetingText() {
+    return element(by.css('app-root p')).getText() as Promise<string>;
   }
 }
